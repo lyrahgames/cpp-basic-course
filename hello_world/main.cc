ff19175000eb65e16d6c66e4e19a7c5fb92daad3
lyrahgames <markuspawellek@gmail.com>
2018-08-23 01:32:31 +0200

    Add description to Hello World example code

--- hello_world/main.cc
+++ hello_world/main.cc
@@ -1,3 +1,20 @@
+// Include the header file of the C++ STL library for input and output
+// operations on the console.
 #include <iostream>
 
-int main() { std::cout << "Hello World!" << std::endl; }
+// The execution of every C++ program starts with the main function.
+// This function is obligatory.
+// The first term defines the return type.
+// For the main function it has to be 'int' for integer.
+// The second term defines the name of the function.
+// In the case of the main function the name must be 'main'.
+// The parentheses enclose optional arguments of the function.
+// Between the curly braces one defines the implementation of the function.
+int main() {
+  // 'std::cout' is the object which is used to put out information on the
+  // console. This is done by using the streaming operator '<<'. Text and
+  // strings are typically enclosed by '"'. The streaming operator '<<' can be
+  // chained. 'std::endl' is an input for streams and defines a newline
+  // character.
+  std::cout << "Hello World!" << std::endl;
+}
